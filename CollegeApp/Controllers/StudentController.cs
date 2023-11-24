2b2b39b9dd0748825a6f6a7ba1b61df69510b205
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-24 17:44:00 +0300

    v-10

--- CollegeApp/Controllers/StudentController.cs
+++ CollegeApp/Controllers/StudentController.cs
@@ -8,15 +8,31 @@ namespace CollegeApp.Controllers
     public class StudentController : ControllerBase
     {
         [HttpGet]
+        [Route("All", Name = "GetAllStudents")]
         public IEnumerable<Student> GetStudentName()
         {
             return CollegeRepository.Students;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet]
+        [Route("{id}", Name = "GetAllStudentById")]
         public Student GetStudentById(int id)
         {
             return CollegeRepository.Students.FirstOrDefault(n => n.Id == id);
+        }
+
+        [HttpGet("{name}", Name = "GetAllStudentByName")]
+        public Student GetStudentByName(string name)
+        {
+            return CollegeRepository.Students.FirstOrDefault(n => n.StudentName == name);
+        }
+
+        [HttpDelete("{id}", Name = "DeleteStudentById")]
+        public bool DeleteStudent(int id)
+        {
+            var student = CollegeRepository.Students.FirstOrDefault(n => n.Id == id);
+            CollegeRepository.Students.Remove(student);
+            return true;
         }
     }
 }
