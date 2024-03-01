8807ba655ba5d1d143aa50696ccd5c4a474e7143
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-03-01 13:34:50 +0300

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using CollegeApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All", Name = "GetAllStudents")]
+        public IEnumerable<Student> GetStudentName()
+        {
+            return CollegeRepository.Students;
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetStudentById")]
+        public Student GetStudentById(int id)
+        {
+            return CollegeRepository
+                    .Students
+                    .Where(n => n.Id == id)
+                    .FirstOrDefault();
+        }
+
+        [HttpGet("{name}", Name = "GetStudentByName")]
+        public Student GetStudentByName(string name)
+        {
+            return CollegeRepository
+                .Students
+                .Where(n => n.StudentName == name)
+                .FirstOrDefault();
+        }
+
+        [HttpDelete("{id}", Name = "DeleteStudentById")]
+        public bool DeleteStudent(int id)
+        {
+            var student = CollegeRepository.Students.Where(n => n.Id == id).FirstOrDefault();
+            CollegeRepository.Students.Remove(student);
+            return true;
+        }
+    }
+}
---
+++ Models/CollegeRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>()
+        {
+            new Student()
+            {
+                Id = 1,
+                StudentName = "Student 1",
+                Email = "student1@app.com",
+                Address = "Street 1"
+            },
+            new Student()
+            {
+                Id = 2,
+                StudentName = "Student 2",
+                Email = "student2@app.com",
+                Address = "Street 2"
+            }
+        };
+    }
+}
